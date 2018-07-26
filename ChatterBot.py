a440eb6b986e209a37cc0a5a564d02e640f8110c
KCSunrise <aroonya@gmail.com>
2018-07-25 21:07:24 -0500

    emove Api keys from Config

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,7 +4,10 @@ import json
 import time
 
 # Twitter API Keys
-
+consumer_key = "Z1ibfzsIajVIQYpGndbOj33Gg"
+consumer_secret = "p5X87DFjZrpRSkPeAq1QcGWbEcJxldjVkOUZcUuDmP08t7T2aN"
+access_token = "366131619-GhQ1mZc4sP5ffobkp5lZjv8pMs9AviHhzrF5wuRq"
+access_token_secret = "OOkErPvCw5OhfwHXcAvBmvP8V0XaNp0up50g52ZMI0iXp"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
